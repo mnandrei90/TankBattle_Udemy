f46c67afe96fd4bd29c924209f9f9d5fb9805633
mnandrei <mnandrei90@gmail.com>
2016-04-04 20:22:42 +0300

    Created base terrain.

---
+++ TankBattleProject/Source/TankBattleProject/TankBattleProjectGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TankBattleProjectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKBATTLEPROJECT_API ATankBattleProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
